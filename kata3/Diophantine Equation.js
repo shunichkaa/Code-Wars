128a32a7c865087719dfb2e0d559caefb2de3a27
aleksandra <matealeksandra@gmail.com>
2025-06-25 18:35:52 +0200

    adding kata and solution

---
+++ kata3/Diophantine Equation.js
@@ -0,0 +1,35 @@
+// In mathematics, a Diophantine equation is a polynomial equation, usually with two or more unknowns, such that only the integer solutions are sought or studied.
+//
+// 	In this kata we want to find all integers x, y (x >= 0, y >= 0) solutions of a diophantine equation of the form:
+//x ˆ2−4y ˆ2=n
+// (where the unknowns are x and y; and n is a given positive number) in decreasing order of the positive
+// x
+// i
+// x
+// i
+// ​
+// .
+//
+// If there is no solution return [] or "[]" or "". (See "RUN SAMPLE TESTS" for examples of returns).
+//
+// Examples:
+// 	90005 --> [[45003, 22501], [9003, 4499], [981, 467], [309, 37]]
+// 90002 --> []
+// Hint: xˆ2 −4yˆ2 =(x−2y)⋅(x+2y)
+
+
+function solequa(n) {
+	const result = [];
+	for (let a = 1; a * a <= n; a++) {
+		if (n % a === 0) {
+			const b = n / a;
+			const x = (a + b) / 2;
+			const y = (b - a) / 4;
+			if (Number.isInteger(x) && Number.isInteger(y) && x >= 0 && y >= 0) {
+				result.push([x, y]);
+			}
+		}
+	}
+	result.sort((p1, p2) => p2[0] - p1[0]);
+	return result;
+}
